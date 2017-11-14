d381ef02502521ef2b569105973197aa36398534
caowenlong <wenlong@nfs.iscas.ac.cn>
2017-11-14 23:55:56 +0800

    <dev>default argument</dev>

---
+++ pyrecipes/mxnet/argument_default.py
@@ -0,0 +1,106 @@
+
+import os
+import argparse
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from common import find_mxnet, data, fit
+from common.util import download_file
+import mxnet as mx
+
+def add_data_args(parser):
+    data = parser.add_argument_group('Data', 'the input images')
+    data.add_argument('--data-train', type=str, help='the training data')
+    data.add_argument('--data-val', type=str, help='the validation data')
+    data.add_argument('--rgb-mean', type=str, default='123.68,116.779,103.939',
+                      help='a tuple of size 3 for the mean rgb')
+    data.add_argument('--pad-size', type=int, default=0,
+                      help='padding the input image')
+    data.add_argument('--image-shape', type=str,
+                      help='the image shape feed into the network, e.g. (3,224,224)')
+    data.add_argument('--num-classes', type=int, help='the number of classes')
+    data.add_argument('--num-examples', type=int, help='the number of training examples')
+    data.add_argument('--data-nthreads', type=int, default=4,
+                      help='number of threads for data decoding')
+    data.add_argument('--benchmark', type=int, default=0,
+                      help='if 1, then feed the network with synthetic data')
+    return data
+
+def add_data_aug_args(parser):
+    aug = parser.add_argument_group(
+        'Image augmentations', 'implemented in src/io/image_aug_default.cc')
+    aug.add_argument('--random-crop', type=int, default=1,
+                     help='if or not randomly crop the image')
+    aug.add_argument('--random-mirror', type=int, default=1,
+                     help='if or not randomly flip horizontally')
+    aug.add_argument('--max-random-h', type=int, default=0,
+                     help='max change of hue, whose range is [0, 180]')
+    aug.add_argument('--max-random-s', type=int, default=0,
+                     help='max change of saturation, whose range is [0, 255]')
+    aug.add_argument('--max-random-l', type=int, default=0,
+                     help='max change of intensity, whose range is [0, 255]')
+    aug.add_argument('--max-random-aspect-ratio', type=float, default=0,
+                     help='max change of aspect ratio, whose range is [0, 1]')
+    aug.add_argument('--max-random-rotate-angle', type=int, default=0,
+                     help='max angle to rotate, whose range is [0, 360]')
+    aug.add_argument('--max-random-shear-ratio', type=float, default=0,
+                     help='max ratio to shear, whose range is [0, 1]')
+    aug.add_argument('--max-random-scale', type=float, default=1,
+                     help='max ratio to scale')
+    aug.add_argument('--min-random-scale', type=float, default=1,
+                     help='min ratio to scale, should >= img_size/input_shape. otherwise use --pad-size')
+    return aug
+
+def set_data_aug_level(aug, level):
+    if level >= 1:
+        aug.set_defaults(random_crop=1, random_mirror=1)
+    if level >= 2:
+        aug.set_defaults(max_random_h=36, max_random_s=50, max_random_l=50)
+    if level >= 3:
+        aug.set_defaults(max_random_rotate_angle=10, max_random_shear_ratio=0.1, max_random_aspect_ratio=0.25)
+
+
+
+
+if __name__ == '__main__':
+    # download data
+#    (train_fname, val_fname) = download_cifar10()
+
+    # parse args
+    parser = argparse.ArgumentParser(description="train cifar100",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    fit.add_fit_args(parser)
+    data.add_data_args(parser)
+    data.add_data_aug_args(parser)
+    data.set_data_aug_level(parser, 2)
+    parser.set_defaults(
+        # network
+        network        = 'cifar10',
+        num_layers     = 8,
+        # data
+        data_train     = '/data/cifar/cifar100/train.rec',
+        data_val       = '/data/cifar/cifar100/test.rec',
+        num_classes    = 100,
+        num_examples   = 50000,
+        image_shape    = '3,32,32',
+        #pad_size       = 4,
+        # train
+        batch_size     = 256,
+        num_epochs     = 200,
+        lr_step_epochs = '50,100,150',
+        optimizer        = 'sgd',
+        disp_batches     = 10,
+        lr               = 0.1,
+        top_k            = 5,
+    )
+    args = parser.parse_args()
+
+    parser.add_argument('--log', dest='log_file', type=str, default="train.log",
+                    help='save training log to file')
+
+    # load network
+    from importlib import import_module
+    net = import_module('symbols.'+args.network)
+    sym = net.get_symbol(**vars(args))
+
+    # train
+    fit.fit(args, sym, data.get_rec_iter)
